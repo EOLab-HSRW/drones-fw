b8f67dfa2085c0667872a18fc15eebe1b493f61e
harleylara <harley.lara@outlook.com>
2024-11-12 14:35:15 +0100

    check for FIX rc_input module disable

--- build.sh
+++ build.sh
@@ -101,7 +101,7 @@ build_drone() {
     case "$drone_name" in
         platypus)
             PX4_VERSION="v1.15.0"
-            DRONE_FW_VERSION="0.0.3"
+            DRONE_FW_VERSION="0.0.4"
 
             VENDOR="px4"
             MODEL="fmu-v3"
