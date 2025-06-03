9d259873eab6510054c0d0da78b3499c424e7849
harleylara <harley.lara@outlook.com>
2025-06-03 23:22:04 +0200

    major refactor

--- eolab_drones/utils.py
+++
@@ -1,9 +0,0 @@
-from argparse import ArgumentTypeError
-from pathlib import Path
-
-def parse_directory(directory: str):
-
-    path = Path(directory)
-    if not path.is_dir():
-        raise ArgumentTypeError(f"{path} is not a valid directory.")
-    return path
