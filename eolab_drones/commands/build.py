cf5561aa9451262abe2c25d8ce578bfebc920aa7
harleylara <harley.lara@outlook.com>
2025-06-09 20:08:04 +0200

    add print output

--- eolab_drones/commands/build.py
+++ eolab_drones/commands/build.py
@@ -1,6 +1,6 @@
 import subprocess
 from argparse import Namespace, ArgumentParser
-from easy_px4.backend import Command, valid_dir_path
+from easy_px4.backend import Command, valid_dir_path, run_command
 from ..api import get_drones, get_drone_path, get_components_path
 
 class BuildDroneCommand(Command):
@@ -30,17 +30,42 @@ class BuildDroneCommand(Command):
                             type=valid_dir_path,
                             help="Output directory to copy PX4 msgs.")
 
+        parser.add_argument("--overwrite",
+                            action="store_true",
+                            help="Overwrite build if present.")
+
     def execute(self, args: Namespace):
 
         build_cmd = ["easy_px4", "build",
                      "--type", args.type,
                      "--path", get_drone_path(args.drone),
-                     "--comps", get_components_path(),
-                     "--msgs-output", args.msgs_output]
+                     "--comps", get_components_path()]
 
-        # subprocess.check_output([])
+        if args.type == "firmware":
+            build_cmd.extend(["--output", "."])
 
-        print(build_cmd)
+        if args.msgs_output:
+            build_cmd.extend(["--msgs-output", str(args.msgs_output.resolve())])
+
+        if args.overwrite:
+            build_cmd.append("--overwrite")
+
+        process = subprocess.Popen(
+            build_cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True  # Ensures output is decoded to string
+        )
+
+        for line in process.stdout:
+            print(line, end='')  # Avoid double newline
+
+        # build_drone = run_command(build_cmd)
+        print(f"\nProcess finished with return code: {return_code}")
+
+        # if build_drone["returncode"] != 0:
+        #     self.logger.error(f"Failed to build firmware. {build_drone['stderr']}, {build_drone['stdout']}")
+
         return None
 
 
