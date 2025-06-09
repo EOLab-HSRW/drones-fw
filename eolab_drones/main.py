22b58524d1588afe088fe778ef85ff3471120823
harleylara <harley.lara@outlook.com>
2025-06-09 18:16:20 +0200

    init cli entrypoint

--- eolab_drones/main.py
+++ eolab_drones/main.py
@@ -1,7 +1,41 @@
 import sys
+import argparse
+
+from easy_px4.backend import Command
+from .commands.build import BuildDroneCommand
+
+# available command registration
+COMMAND_REGISTRY: list[type[Command]] = [
+    BuildDroneCommand
+
+]
 
 def main():
+
+    parser = argparse.ArgumentParser(
+        prog=__package__,
+
+        description="A simple tool to help building custom PX4-firmwares"
+    )
+
+    subparsers = parser.add_subparsers(dest="command", required=True, help="Available commands")
+
+    cmd_register = {}
+
+    for command_class in COMMAND_REGISTRY:
+        subparser = subparsers.add_parser(command_class.cmd_name, help=f"{command_class.cmd_name} command")
+        command_class().add_arguments(subparser)
+        cmd_register[command_class.cmd_name] = command_class
+
+    args = parser.parse_args()
+
+
+    with cmd_register[args.command]() as worker:
+        worker.execute(args)
+
+
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
